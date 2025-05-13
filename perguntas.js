da41bcc8987a3793f9e588b208fe20aa1078848e
thiagotessari <t.tessari@escola.pr.gov.br>
2025-05-13 14:28:04 +0000

    ok

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Programação',
+    'O que é HTML?',
+    resposta,
+)
+
+criaCartao(
+    'Inglês',
+    'Como é dia do pagamento?',
+    'Pay Day',
+)
+
+criaCartao(
+    categoria,
+    pergunta,
+    resposta,
+)
